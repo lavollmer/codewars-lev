128e9cb24e23daeb8c159af054f982fad7a5fa29
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-03-01 18:08:44 -0600

    README and update on code

--- codewars.js
+++ codewars.js
@@ -1,4 +1,4 @@
-function lovefunc(flower1, flower2){
+function lovefunc(flower1, flower2) {
   // moment of truth
   // TRUE = flower1 EVEN AND flower2 ODD
   // FALSE = flower1 ODD AND flower2 ODD
@@ -6,17 +6,15 @@ function lovefunc(flower1, flower2){
   // TRUE = flower1 ODD AND flower2 EVEN
 
   //modulus divides and it is even if the modulus returns zero
- function isEven(flower){
-
+  function isEven(flower) {
     return flower % 2 === 0;
   }
-  
-   //modulus divides and it is odd if the modulus returns anything other zero
-function isOdd (flower){
 
+  //modulus divides and it is odd if the modulus returns anything other zero
+  function isOdd(flower) {
     return flower % 2 !== 0;
   }
-  
+
   //if else statement ==> if flower1 is true and flower2 is true
 
   if (isEven(flower1) && isOdd(flower2)) {
@@ -26,3 +24,4 @@ function isOdd (flower){
   } else {
     return false;
   }
+}
